5646acb769fbee6652ac9805404df87786eb8d53
Dzy-hue <diazprahadyan999@gmail.com>
2026-09-16 22:12:51 +0700

    Latihan Tambahan 3 Perluas initTableFilter

--- Jobsheet-05/assets/js/app.js
+++ Jobsheet-05/assets/js/app.js
@@ -33,8 +33,9 @@ function initTableFilter() {
         const Keyword = input.value.toLowerCase();
         const rows = table.querySelectorAll("tbody tr");
         rows.forEach(function (row) {
-            const teks = row.textContent.toLowerCase();
-            row.style.display = teks.includes(Keyword) ? "" : "none";
+            const kolomJudul = row.querySelector("td");
+            const teksJudul = kolomJudul ? kolomJudul.textContent.toLowerCase() : "";
+            row.style.display = teksJudul.includes(Keyword) ? "" : "none";
         });
     });
 }
