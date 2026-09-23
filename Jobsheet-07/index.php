5be328bd87ce9cce5fca41134cff6f0b0531ea30
Dzy-hue <diazprahadyan999@gmail.com>
2026-09-24 05:58:09 +0700

    1. Update index.php, includes/header.php & includes/footer.php

---
+++ Jobsheet-07/index.php
@@ -0,0 +1,9 @@
+<?php
+$page_title = "Beranda";
+include __DIR__ . '/includes/header.php';
+?>
+        <section>
+            <h2>Selamat Datang di Sistem Perpustakaan Mini</h2>
+            <p>Kelola data koleksi buku dan keanggotaan perpustakaan dengan mudah.</p>
+        </section>
+<?php include __DIR__ . '/includes/footer.php'; ?>
